75dc350eb6628419503a7a3f14dbd067ed2d473f
ClDaly2904 <c.l.daly@hotmail.com>
2023-01-25 16:02:24 +0000

    Add home app and add templates

--- beauty_broker/urls.py
+++ beauty_broker/urls.py
@@ -1,7 +1,6 @@
 """beauty_broker URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -19,4 +18,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('', include('home.urls')),
 ]
