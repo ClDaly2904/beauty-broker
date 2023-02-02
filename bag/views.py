198fb08d40cadd9b0d1600a9d27c867caf7839d7
ClDaly2904 <c.l.daly@hotmail.com>
2023-02-02 17:50:24 +0000

    Add add to bag functionality. Update user description to accordian

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 # Create your views here.
@@ -6,3 +6,22 @@ def view_bag(request):
     """ A view to render the users shopping bag page """
 
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, item_id):
+    """ Add a specified quantity of the product
+    to the users shopping bag """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+
+    else:
+        bag[item_id] = quantity
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
