7d37da1b7c02c11d584e55ed0b495fd375a77239
ClDaly2904 <c.l.daly@hotmail.com>
2023-02-04 17:48:06 +0000

    Add bag tools and register filter to calculate subtotal for individual products in shopping bag

---
+++ bag/templatetags/bag_tools.py
@@ -0,0 +1,10 @@
+from django import template
+
+
+register = template.Library()
+
+@register.filter(name='calc_subtotal')
+def calc_subtotal(price, quantity):
+    """ Calculate subtotal of a product for bag template """
+
+    return price * quantity
