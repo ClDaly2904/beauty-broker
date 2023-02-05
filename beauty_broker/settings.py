7129fc63812c3060f06134bbc7c166bf7918a51f
ClDaly2904 <c.l.daly@hotmail.com>
2023-02-05 18:35:07 +0000

    Create checkout urls, template and css for checkout app

--- beauty_broker/settings.py
+++ beauty_broker/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -60,6 +61,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'beauty_broker.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -77,6 +80,11 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
             ],
+            # access crispy forms from all fields by default
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field'
+            ]
         },
     },
 ]
--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from .forms import OrderForm
 
-# Create your views here.
+
+def checkout(request):
+    """ Render checkout page with order form """
+
+    # retrieve the bag from the session
+    bag = request.session.get('bag', {})
+
+    # prevent url from being accessed manually if bag empty
+    if not bag:
+        messages.error(request, "Sorry, your bag is empty!")
+        return redirect(reverse('products'))
+
+    # create instance of order form
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
