acbc0e51faecad7db1e6f4a354055866d29f35a1
ClDaly2904 <c.l.daly@hotmail.com>
2023-02-07 17:21:51 +0000

    Add checkout form data caching in payment intent

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -8,32 +8,29 @@ from checkout.webhook_handler import StripeWH_Handler
 import stripe
 
 
-# reject GET requests
 @require_POST
-# make CSRF exempt as Stripe doesn't send one
 @csrf_exempt
 def webhook(request):
     """ Listen for webhooks from Stripe """
-
     # Setup
     wh_secret = settings.STRIPE_WH_SECRET
     stripe.api_key = settings.STRIPE_SECRET_KEY
 
-    # Get the webhook data, verify its signature
+    # Get webhook data, verify its signature
     payload = request.body
     sig_header = request.META['HTTP_STRIPE_SIGNATURE']
     event = None
 
     try:
         event = stripe.Webhook.construct_event(
-            payload, sig_header, wh_secret
+                payload, sig_header, wh_secret
         )
     except ValueError as e:
         # Invalid payload
-        return HttpResponse(content=e, status=400)
+        return HttpResponse(status=400)
     except stripe.error.SignatureVerificationError as e:
         # Invalid signature
-        return HttpResponse(content=e, status=400)
+        return HttpResponse(status=400)
     except Exception as e:
         return HttpResponse(content=e, status=400)
 
@@ -44,7 +41,8 @@ def webhook(request):
     event_map = {
         'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
         'payment_intent.payment_failed': (handler.
-                                          handle_payment_intent_payment_failed)
+                                          handle_payment_intent_payment_failed
+                                          ),
     }
 
     # Get the webhook type from Stripe
