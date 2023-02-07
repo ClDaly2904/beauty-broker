738cfadb928ae35fa4127b8fe0515fade40d3186
ClDaly2904 <c.l.daly@hotmail.com>
2023-02-07 19:16:22 +0000

    Complete stripe webhook handler

--- checkout/views.py
+++ checkout/views.py
@@ -59,7 +59,12 @@ def checkout(request):
         order_form = OrderForm(form_data)
 
         if order_form.is_valid():
-            order = order_form.save()
+            # prevent multiple save events
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+            order.save()
             # iterate through bag items to create line item
             for item_id, item_data in bag.items():
                 try:
