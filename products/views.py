5882686c14c61e00388d4496ba5182d0fcc87a6a
ClDaly2904 <c.l.daly@hotmail.com>
2023-02-08 20:45:12 +0000

    Add Update product CRUD functionality for store owners

--- products/views.py
+++ products/views.py
@@ -114,3 +114,34 @@ def add_product(request):
     }
 
     return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    """ A view for store owners to edit an existing product """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    if request.method == 'POST':
+
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated product!')
+            # return user to the newly updated product page
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to update product information.\n'
+                                    ' Please ensure the information provided\n'
+                                    'is valid.')
+
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'product': product,
+        'form': form,
+    }
+
+    return render(request, template, context)
