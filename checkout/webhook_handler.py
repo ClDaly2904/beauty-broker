67f6597a569cc8300b89bfddaae9857d103ab890
ClDaly2904 <c.l.daly@hotmail.com>
2023-02-08 18:38:20 +0000

    Update checkout webhook handler to handle profiles

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 
 from .models import Order, OrderLineItem
 from products.models import Product
+from profiles.models import UserProfile
 
 import stripe
 import json
@@ -43,8 +44,26 @@ class StripeWH_Handler:
             if value == "":
                 shipping_details.address[field] = None
 
+        # Update profile information if save_info was checked
+        profile = None
+        username = intent.metadata.username
+        if username != 'AnonymousUser':
+            profile = UserProfile.objects.get(user__username=username)
+            if save_info:
+                profile.default_phone_number = shipping_details.phone
+                profile.default_country = shipping_details.address.country
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_town_or_city = shipping_details.address.city
+                profile.default_street_address1 = (shipping_details
+                                                   .address.line1)
+                profile.default_street_address2 = (shipping_details
+                                                   .address.line2)
+                profile.default_county = shipping_details.address.state
+                profile.save()
+
         order_exists = False
         attempt = 1
+
         while attempt <= 5:
             try:
                 order = Order.objects.get(
@@ -63,19 +82,25 @@ class StripeWH_Handler:
                 )
                 order_exists = True
                 break
+
             except Order.DoesNotExist:
                 attempt += 1
                 time.sleep(1)
+
         if order_exists:
             return HttpResponse(
                 content=(f'Webhook received: {event["type"]} | SUCCESS: '
                          'Verified order already in database'),
                 status=200)
+
         else:
             order = None
+
             try:
                 order = Order.objects.create(
                     full_name=shipping_details.name,
+                    # relate order to user in webhook
+                    user_profile=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,
@@ -87,6 +112,7 @@ class StripeWH_Handler:
                     original_bag=bag,
                     stripe_pid=pid,
                 )
+
                 for item_id, item_data in json.loads(bag).items():
                     product = Product.objects.get(id=item_id)
                     if isinstance(item_data, int):
@@ -97,6 +123,7 @@ class StripeWH_Handler:
                         )
                         order_line_item.save()
                     else:
+
                         for size, quantity in (item_data['items_by_size']
                                                .items()):
                             order_line_item = OrderLineItem(
@@ -106,12 +133,15 @@ class StripeWH_Handler:
                                 product_size=size,
                             )
                             order_line_item.save()
+
             except Exception as e:
+
                 if order:
                     order.delete()
                 return HttpResponse(
                     content=f'Webhook received: {event["type"]} | ERROR: {e}',
                     status=500)
+
         self._send_confirmation_email(order)
         return HttpResponse(
             content=(f'Webhook received: {event["type"]} | SUCCESS: '
