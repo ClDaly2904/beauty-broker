73f2cf792649070f7082d58acf7d34373423e0bb
ClDaly2904 <c.l.daly@hotmail.com>
2023-02-08 20:14:21 +0000

    Complete add product CRUD functionality

--- products/views.py
+++ products/views.py
@@ -96,7 +96,18 @@ def product_detail(request, product_id):
 
 def add_product(request):
     """ A view for store owners to add a product """
-    form = ProductForm()
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure\n'
+                                    'the information provided is valid.')
+    else:
+        form = ProductForm()
     template = 'products/add_product.html'
     context = {
         'form': form,
