fc4d1ec4d2b47d5cc48e3348ad89afa7a5b42f58
ClDaly2904 <c.l.daly@hotmail.com>
2023-02-10 10:04:56 +0000

    Fix default email and name not saving to profile and checkout form

--- checkout/views.py
+++ checkout/views.py
@@ -168,6 +168,8 @@ def checkout_success(request, order_number):
         # save info as default for profile if box ticked
         if save_info:
             profile_data = {
+                'default_full_name': order.full_name,
+                'default_email': order.email,
                 'default_phone_number': order.phone_number,
                 'default_country': order.country,
                 'default_postcode': order.postcode,
